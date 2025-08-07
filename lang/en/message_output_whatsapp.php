631844559745e0c8eefe012ac41babc5b1c6cfb7
fabrizio <fabrizio.machado@gmail.com>
2025-08-07 11:18:47 -0300

    Update message_output_whatsapp.php

--- lang/en/message_output_whatsapp.php
+++ lang/en/message_output_whatsapp.php
@@ -1,3 +1,27 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ *  Whatsapp message processor language information
+ *
+ * @package    message_whatsapp
+ * @copyright  2022 Cognize Learning
+ * @author     Abhishek Kumar <abhishek@cognizelearning.com>
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
 $string['pluginname'] = 'WhatsApp Notifications';
 $string['configtitle'] = 'WhatsApp Configuration';
 $string['api'] = 'API WhatsApp';
@@ -7,3 +31,35 @@ $string['apiurl_desc'] = 'Endpoint de envio de mensagens.';
 $string['twilio_sid'] = 'SID da conta Twilio';
 $string['twilio_token'] = 'Token da conta Twilio';
 $string['twilio_from'] = 'Número autorizado Twilio (whatsapp:+55...)';
+
+
+
+# $string['pluginname'] = 'Whatsapp';
+$string['accountsid'] = 'Account SID';
+$string['accountsid_desc'] = 'Twillio Account SID';
+$string['authtoken'] = 'Auth Token';
+$string['authtoken_desc'] = 'Twillio Auth Token';
+$string['senderno'] = 'Sender Whatsapp No';
+$string[''] = '';
+$string[''] = '';
+$string[''] = '';
+$string[''] = '';
+$string[''] = '';
+$string[''] = '';
+$string[''] = '';
+$string[''] = '';
+$string[''] = '';
+$string[''] = '';
+$string[''] = '';
+$string[''] = '';
+$string[''] = '';
+$string[''] = '';
+$string[''] = '';
+$string[''] = '';
+$string[''] = '';
+$string[''] = '';
+$string[''] = '';
+$string[''] = '';
+$string[''] = '';
+$string[''] = '';
+$string[''] = '';
